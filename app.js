74004ac5340d8b462a3422ccd2ad14f2f488afc2
stevenremenapp <remenapp@gmail.com>
2019-01-12 11:49:24 -0500

    Working skeleton of node portfolio site

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(3000, function() {
+    console.log('running running');
+});
